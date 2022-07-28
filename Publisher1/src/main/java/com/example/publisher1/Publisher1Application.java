aef0f8b5b761bd9598b0df84c12aa9933a9420d4
Rubi221 <98795896+Rubi221@users.noreply.github.com>
2022-07-27 22:29:52 -0500

    Add files via upload

---
+++ Publisher1/src/main/java/com/example/publisher1/Publisher1Application.java
@@ -0,0 +1,13 @@
+package com.example.publisher1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Publisher1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Publisher1Application.class, args);
+    }
+
+}
---
+++ Publisher1/src/test/java/com/example/publisher1/Publisher1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.publisher1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Publisher1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
